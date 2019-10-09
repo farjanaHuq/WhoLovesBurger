59fb121ab465f96c2d465351ab46e36cce71b440
Farjana Huq <fa.huq03@gmail.com>
2019-10-09 06:38:13 -0700

    WhoLovesBurger

---
+++ model/userModel.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+   firstName: {
+      type: String,
+      trim: true,
+      required: true
+   },
+   lastName: {
+      type: String,
+      trim: true,
+      required: true
+   },
+   displayName: {
+      type: String,
+      trim: true,
+      required: true
+   },
+   password:{
+      type: String,
+      trim: true,
+      required: true,
+      validate: [
+         function(input){
+            return input.length>=6;
+         },
+         "Password should be longer the 6 character."
+      ]
+   },
+   email: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+      match: [/\S+@\S+\.\S+/, "Please enter a valid email address."]
+   },
+   userCreated: {
+      type: Date,
+      default: Date.now
+   },
+   lastUpdated:{type:Date}, 
+   // fullName: String,
+   emailValidated: {
+      type: Boolean,
+      default: false
+   }
+});
+// userSchema.method.getFullName = function(){
+//    this.fullName = this.firstName + " " + this.lastName;
+//    return this.fullName;
+// },
+// userSchema.method.lastUpdatedDate = function(){
+//    this.lastUpdated = Date.now();
+//    return this.lastUpdated;
+// }
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,16 +1,53 @@
+//dependencies
 var express = require('express');
 var app = express();
-
 var path = require('path');
+var logger = require('morgan');
 var mongoose = require('mongoose');
 
+var User = require("./model/userModel");
+
+app.use(express.static('public'));
+
+//Use mongoose looger and promise
+app.use(logger('dev'));
+// Define middleware here
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
 // Define any API routes before this runs
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+//save user to the mongoDB
+app.post("/submit", function(req,res){
+    // var user = new userModel(req.body)
+    // user.getFullName();
+    // user.lastUpdatedDate();
+    // Create a new user using req.body
+  User.create(req.body)
+  .then(function(dbUser) {
+    // If saved successfully, send the the new User document to the client
+    res.json(dbUser);
+  })
+  .catch(function(err) {
+    // If an error occurs, send the error to the client
+    res.json(err);
+  });
+
+})
+
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/burgerDB", {useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/burgerDB",{useNewUrlParser: true })
+.then(() => console.log('DB Connected!'))
+.catch(err => {
+console.log(`DB Connection Error: ${err.message}`)});
+
+//To get rid of the warning: DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead.
+mongoose.set('useCreateIndex', true);
 
 var PORT = process.env.PORT || 8080;
 // Start our server so that it can begin listening to client requests.
@@ -18,3 +55,6 @@ app.listen(PORT, function() {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
+
+
+//db.getCollection('user').find({})
